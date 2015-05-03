e71c50724ac8e5d64cb77a13c7707e0a33d24f17
Fedot <fedotru@gmail.com>
2015-05-03 21:31:56 +0300

    Update compress.sh

--- compress.sh
+++ compress.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-tar --exclude-vcs --exclude="docker" --exclude="logs/*" --exclude="cache/*" --exclude=".idea" -cavf docker/sources.tar.bz2 .
+tar --exclude-vcs --exclude="docker" --exclude="./logs/*" --exclude="./cache/*" --exclude=".idea" -cavf docker/sources.tar.bz2 .
 
